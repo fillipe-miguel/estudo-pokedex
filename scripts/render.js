48e0a851f02fe31424464563a360b3b972f822bf
Fillipe <fillipeolipe@gmail.com>
2022-03-10 22:18:08 -0300

    Adicionando a função de ver todos os pokemons

--- scripts/render.js
+++ scripts/render.js
@@ -5,7 +5,7 @@ render = {
 		// NÃO ESTOU USANDO A API PARA RETORNAR AS IMAGENS!!!!!!!!
 		let id = ('000' + pokemon.id).slice(-3);
 
-		let post = `<div class="card_pokemon">
+		let post = `<div class="card_pokemon normal">
         <h2>Nome: ${pokemon.name} N°${pokemon.id}</h2>
         <div id="card_info">
             <div id="poke_img">
@@ -34,6 +34,7 @@ render = {
 
 		let divpokes = document.getElementById('pokes');
 		divpokes.innerHTML = post;
+		divpokes.dataset.represent = 'unique';
 	},
 
 	type(pokemon) {
@@ -70,40 +71,3 @@ render = {
 		return allMoviments;
 	},
 };
-
-{
-	/* <div class="card mb-5 container mt-5">
-	<div class="card-body">
-		<h5 class="card-title">Nome do pokemon: Pikachu</h5>
-		<p class="card-text">Id do Pikachu: 25</p>
-		<h5 class="card-title">All moviments:</h5>
-	</div>
-	<div class="card mb-2">
-		<div class="card-body">
-			<h5 class="card-title">Movimento : Mega-punsh</h5>
-			<p class="card-text">Level para aprender: 0</p>
-			<p class="card-text">Metodo de aprendizagem: machine</p>
-			<p class="card-text">Versão do jogo: red-blue</p>
-		</div>
-	</div>
-</div>; */
-}
-
-{
-	/* <div class="card-body">
-                        <h5 class="card-title">Nome do pokemon: ${
-							pokemon.name
-						}</h5>
-                        <p class="card-text">Id do ${pokemon.name}: ${
-			pokemon.id
-		}</p>
-                        <p class="card-text">Abilidades: ${this.abilities(
-							pokemon
-						)}</p>
-                        <p class="card-text">Altura:  cm</p>
-                        <p class="card-text">Peso: ${pokemon.weight / 10} kg</p>
-                        <p class="card-text">Tipos:</p>
-                        ${this.type(pokemon)}
-                        <h5 class="card-title">All moviments:</h5>
-                    </div> */
-}
---
+++ scripts/showall.js
@@ -0,0 +1,42 @@
+var search = 0;
+var allcards = '';
+function showall() {
+	let pokesDiv = document.getElementById('pokes');
+
+	if (pokesDiv.dataset.represent == 'unique' || pokesDiv.innerHTML == '') {
+		pokesDiv.innerHTML = `<div id="pokes_all" class="card_pokemon all"></div><div class="d-grid gap-2 mt-2"><button onclick="showall()" class="btn btn-primary" type="button" >Carregar mais</button></div>`;
+		pokesDiv.dataset.represent = 'all';
+	}
+
+	let options = {
+		metodh: 'GET',
+	};
+
+	for (var i = 0; i < 3; i++) {
+		search += 1;
+
+		let id = ('000' + search).slice(-3);
+
+		console.log(`search: ${search} ${i}`);
+
+		fetch(`https://pokeapi.co/api/v2/pokemon/${search}`, options)
+			.then((response) => {
+				return response.json();
+			})
+			.then((pokemon) => {
+				allcards += `<div class="information"><img id="poke_img" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png" / > <h2>${pokemon.name}</h2> </div>
+				`;
+			});
+
+		if (i == 2) {
+			setTimeout(() => {
+				document.getElementById('pokes_all').innerHTML = allcards;
+				console.log('estou auiq');
+			}, 100);
+		}
+	}
+}
+
+{
+	/* <div class="card_pokemon all"></div>; */
+}
